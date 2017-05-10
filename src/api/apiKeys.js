d9e47a07cb99f6c70a97859936760818937cb3c8
Gabriel Duarte <gabrielqduarte@gmail.com>
2017-05-10 00:31:44 -0300

    Update apiKeys.js

--- src/api/apiKeys.js
+++ src/api/apiKeys.js
@@ -1,7 +1,3 @@
 export default {
-  trakt: {
-    clientId: 'cc037749db23fbe41a9dcdb79f5eb7c9d9345552bf212c707da361692346de55',
-    clientSecret: '3e7d77022f23993185497e33b7650a7a219aaeb312131c0a79aac55ffc56cb56'
-  },
-  tmdb: 'a41efefdade1a5130dd72cc2dcd64044'
+  tmdb: '[api_key]'
 }
