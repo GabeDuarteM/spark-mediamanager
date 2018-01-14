9bb0cd23e4ed317de94d4b397167fd86e5956e43
Gabriel Duarte <gabrielqduarte@gmail.com>
2018-01-14 18:28:50 -0200

    bump packages

--- src/containers/PosterListContainer/PosterListContainer.test.tsx
+++ src/containers/PosterListContainer/PosterListContainer.test.tsx
@@ -79,25 +79,6 @@ describe("COMPONENT: <PosterListContainer />", () => {
     shallow(component)
   })
 
-  it("should throw an error if visibilityFilter is unknown", () => {
-    const initialState: IStoreState = {
-      video: {
-        series: [...returnMockSerie()],
-        movies: [...returnMockMovie()],
-        animes: [returnMockAnime()[0], ...returnMockAnime()],
-        visibilityFilter: "unknown" as EVideoType,
-      },
-      editVideo: {},
-    }
-    const store = configureStore(initialState)
-    const component = (
-      <AppWrapper store={store} locale="en">
-        <PosterListContainer />
-      </AppWrapper>
-    )
-    expect(() => mount(component)).toThrow("An invalid visibilityFilter were supplied to setVisibleVideos")
-  })
-
   it("should fill the editVideo state if a poster is clicked", () => {
     const initialState: IStoreState = {
       video: {
@@ -115,11 +96,32 @@ describe("COMPONENT: <PosterListContainer />", () => {
       </AppWrapper>
     )
     const wrapper = mount(component)
+    const pl = PosterList
+    const l = Link
     wrapper
-      .find(PosterList)
-      .find(Link)
+      .find(pl)
+      .find(l)
       .at(0)
       .simulate("click")
     expect(store.getState().editVideo.video).toBe(initialState.video.animes[0])
+  })
+
+  it("should throw an error if visibilityFilter is unknown", () => {
+    const initialState: IStoreState = {
+      video: {
+        series: [...returnMockSerie()],
+        movies: [...returnMockMovie()],
+        animes: [returnMockAnime()[0], ...returnMockAnime()],
+        visibilityFilter: "unknown" as EVideoType,
+      },
+      editVideo: {},
+    }
+    const store = configureStore(initialState)
+    const component = (
+      <AppWrapper store={store} locale="en">
+        <PosterListContainer />
+      </AppWrapper>
+    )
+    expect(() => mount(component)).toThrow("An invalid visibilityFilter were supplied to setVisibleVideos")
   })
 })
